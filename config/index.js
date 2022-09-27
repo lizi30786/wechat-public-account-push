a8b2dcb35c1bb2de11db6eb96afcc658c70a8965
lizi30786 <55841487+lizi30786@users.noreply.github.com>
2022-09-27 22:24:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -59,14 +59,16 @@ export const config = {
     },
     {
       name: "李子",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oQ_S75wgAz6CfboIFVHtDIAsck0Q",
-     useTemplateId: "jkLJv_qPepQS-YtaIibNi5nipg4K5ynb2I8nmv0hEQk",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "jkLJv_qPepQS-YtaIibNi5nipg4K5ynb2I8nmv0hEQk",
       // 所在省份
       province: "重庆",
       // 所在城市
       city: "重庆",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '05-01',
+      horoscopeDate: '02-20',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -88,7 +90,7 @@ export const config = {
         {"keyword": "marry_day", date: "2022-04-06"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
-      ]
+      ]   
     },
     {
       name: "老婆2",
